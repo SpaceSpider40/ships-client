333713a078971f8dfb6d53111a93cba5b060f0ad
Oskar Ochal <spacespiderproductions@gmail.com>
2024-12-13 17:37:14 +0100

    Add main menu UI and update build settings
    Introduced a main menu UI with styled components and interactive functionality in HTML, CSS, and TypeScript. Configured VCS to use Git and updated package.json to enable watch mode for TypeScript builds. Also included font and styling adjustments to enhance the user interface.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+function selectionIndicator(self: HTMLElement, show: boolean): void {
+    if (show)
+        self.textContent = `> ${self.textContent ?? ''}`
+    else
+        self.textContent = (self.textContent ?? '').replace(/>\s*/, '')
+}
+
+function hide(self: HTMLElement) :void{
+    self.classList.add('hide');
+}
+
+function show(self: HTMLElement) :void{
+    self.classList.remove('hide');
+}
+
+
