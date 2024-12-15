eafa7de5460988cecaf875355ba4aacb8c77ea1d
Oskar Ochal <spacespiderproductions@gmail.com>
2024-12-15 23:16:13 +0100

    Add room menu UI and page switching functionality
    Created a new room menu interface with relevant styling and integrated it into the application. Refactored CSS to include utility styles and optimized "main.css." Implemented page switching logic in JavaScript to handle navigation between the main menu and room menu.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,26 @@ function selectionIndicator(self: HTMLElement, show: boolean): void {
         self.textContent = (self.textContent ?? '').replace(/>\s*/, '')
 }
 
-function hide(self: HTMLElement) :void{
+function hide(self: HTMLElement): void {
     self.classList.add('hide');
 }
 
-function show(self: HTMLElement) :void{
+function show(self: HTMLElement): void {
     self.classList.remove('hide');
 }
 
+let currentPageId: string = 'main-menu';
 
+function switchPage(pageId: string): void {
+    const element = document.getElementById(pageId);
+
+    if (element) {
+        show(element);
+        const currentElement = document.getElementById(currentPageId);
+        if (currentElement) {
+            hide(currentElement)
+        }
+
+        currentPageId = pageId;
+    }
+}
